9291bac5bbea266a8197cc5f8fa9285a49243c35
乐天居易 <19836788252@163.com>
2024-07-30 22:12:56 +0800

    向module.exports对象上挂载属性和方法

--- day02/11.自定义模块.js
+++ day02/11.自定义模块.js
@@ -1,1 +1,9 @@
 //在一个自定义模块中，默认情况下，module.exports ={}
+const age = 20
+//向module.exports对象上挂载username属性
+module.exports.username = "zs"
+//向module.exports对象上挂载sayHello方法
+module.exports.sayHello = function () {
+  console.log("Hello")
+}
+module.exports.age = age
