02679c04f4b3de138079c6d87e1f934bab5aa236
乐天居易 <19836788252@163.com>
2024-08-01 17:59:34 +0800

    手写函数格式化时间

--- day02/13.exports对象.js
+++ day02/13.exports对象.js
@@ -2,14 +2,40 @@
 // console.log(module.exports)
 
 // console.log(exports === module.exports)
-const username = "zs"
+// -----------------------------------------------
 
-module.exports.username = username
+// const username = "zs"
 
-exports.age = 12
+// module.exports.username = username
 
-exports.sayHello = function () {
-  console.log("大家好")
-}
+// exports.age = 12
 
-//最终，向外共享的结果，永远都是module.exports所指向的对多
+// exports.sayHello = function () {
+//   console.log("大家好")
+// }
+
+// // { username: 'zs', age: 12, sayHello: [Function (anonymous)] }
+
+// --------------------------------------------------
+//最终，向外共享的结果，永远都是module.exports所指向的对象
+
+// --------------------------------------------------
+
+// module.exports.username = "zs"
+// exports = {
+//   gender: "男",
+//   age: 22,
+// }
+// // { username: 'zs' }
+
+// --------------------------------------------------
+
+// --------------------------------------------------
+
+// exports.username = "zs"
+// module.exports = {
+//   gender: "男",
+//   age: 22,
+// }
+// // { gender: '男', age: 22 }
+// --------------------------------------------------
