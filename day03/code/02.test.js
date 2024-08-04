329c825037e0eaec7018c18379ce33e474c5f8da
乐天居易 <19836788252@163.com>
2024-08-04 10:38:06 +0800

    发布包-转义&还原HTML的方法

--- day03/code/02.test.js
+++ day03/code/02.test.js
@@ -3,3 +3,12 @@ const heima = require("./heima-tools")
 // 格式化时间的功能
 const dtStr = heima.dateFormat(new Date())
 console.log(dtStr)
+
+// 转义HTML的方法
+const htmlStr = '<h1 title="abc">这是h1标签<span>123&nbsp;</span></h1>'
+const str = heima.htmlEscape(htmlStr)
+console.log(str)
+
+// 还原HTML的方法
+const str1 = heima.htmlUnEscape(str)
+console.log(str1)
--- day03/code/heima-tools/index.js
+++ day03/code/heima-tools/index.js
@@ -17,6 +17,39 @@ function padZero(n) {
   return n > 9 ? n : "0" + n
 }
 
+// 定义转义HTML的方法
+function htmlEscape(htmlStr) {
+  return htmlStr.replace(/<|>|"|&/g, (match) => {
+    switch (match) {
+      case "<":
+        return "&lt;"
+      case ">":
+        return "&gt;"
+      case '"':
+        return "&quot;"
+      case "&":
+        return "&amp;"
+    }
+  })
+}
+// 定义还原HTML的方法
+function htmlUnEscape(htmlStr) {
+  return htmlStr.replace(/&lt;|&gt;|&quot;|&amp;/g, (match) => {
+    switch (match) {
+      case "&lt;":
+        return "<"
+      case "&gt;":
+        return ">"
+      case "&quot;":
+        return '"'
+      case "&amp;":
+        return "&"
+    }
+  })
+}
+
 module.exports = {
   dateFormat,
+  htmlEscape,
+  htmlUnEscape,
 }
