7514e597725c22111d0b7def9d05ec3117756438
乐天居易 <19836788252@163.com>
2024-08-06 23:31:46 +0800

    express-监听GET和POST请求&处理参数

--- day03/code/07.使用express创建最基本的服务器.js
+++ day03/code/07.使用express创建最基本的服务器.js
@@ -2,7 +2,31 @@
 const express = require("express")
 // 2.创建web服务器
 const app = express()
+
+// 4.监听客户端的GET和POST请求，并向客户端响应具体的内容
+app.get("/user", (req, res) => {
+  // 调用express提供的res.send()方法，向客户端响应一个 JSoN对象
+  res.send({ name: "zs", age: 20, gender: "男" })
+})
+app.post("/user", (req, res) => {
+  // 调用express提供的res.send()方法，向客户端响应一个文本字符串
+  res.send("请求成功")
+})
+app.get("/", (req, res) => {
+  // 通过req.query可以获取到客户端发送过来的查询参数
+  // 注意：默认情况下，req.query是一个空对象
+  console.log(req.query)
+  res.send(req.query)
+})
+
+// 注意：这里的：id是一个动态的参数
+app.get("/user/:id/:name", (req, res) => {
+  // req.params是动态匹配到的URL参数，默认也是一个空对象
+  console.log(req.params)
+  res.send(req.params)
+})
+
 // 3.启动web服务器
 app.listen(80, () => {
-  console.log("express server running at http://localhost")
+  console.log("express server running at http://127.0.0.1")
 })
