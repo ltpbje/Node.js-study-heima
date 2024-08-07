a0e5100f65acba6042ff23bec6cf514f89b1ba24
乐天居易 <19836788252@163.com>
2024-08-07 17:51:16 +0800

    express-安装并使用nodemon

--- day03/code/08.使用express.static对外提供静态资源.js
+++ day03/code/08.使用express.static对外提供静态资源.js
@@ -5,8 +5,8 @@ const app = express()
 // 创建一个 express 应用
 
 // 调用 express.static() 方法，指定静态资源目录
-app.use("/files", express.static("./files"))
 app.use(express.static("./clock"))
+app.use("/files", express.static("./files"))
 
 app.listen(80, (req, res) => {
   // 使用 app.listen 监听 80 端口，并打印启动信息
