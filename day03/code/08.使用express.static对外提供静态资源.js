1252fa4c6c995d431138ee25c2ab631d6a3a29c0
乐天居易 <19836788252@163.com>
2024-08-07 17:36:17 +0800

    express-静态资源处理

---
+++ day03/code/08.使用express.static对外提供静态资源.js
@@ -0,0 +1,14 @@
+const express = require("express")
+// 导入 express 模块
+
+const app = express()
+// 创建一个 express 应用
+
+// 调用 express.static() 方法，指定静态资源目录
+app.use("/files", express.static("./files"))
+app.use(express.static("./clock"))
+
+app.listen(80, (req, res) => {
+  // 使用 app.listen 监听 80 端口，并打印启动信息
+  console.log("express server running at http://127.0.0.1")
+})
