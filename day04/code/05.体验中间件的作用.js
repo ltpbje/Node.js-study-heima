1041c5e00f6ab18cf7d4b801b3bea79b72dc05a2
乐天居易 <19836788252@163.com>
2024-08-09 14:48:58 +0800

    体验中间件的作用

--- day04/code/05.体验中间件的作用.js
+++ day04/code/05.体验中间件的作用.js
@@ -14,18 +14,20 @@ const mw = function (req, res, next) {
 
 // 全局中间件的简化形式
 app.use((req, res, next) => {
-  console.log("这是全局中间件函数的简化形式")
+  const time = Date.now()
+  //   为req对象，挂载自定义属性，从而把时间共享给后面的所有路由
+  req.startTime = time
   next()
 })
 
 app.get("/", (req, res) => {
   console.log("调用了 / 这个路由")
 
-  res.send("Home page")
+  res.send("Home page" + req.startTime)
 })
 
 app.get("/user", (req, res) => {
-  res.send("User page")
+  res.send("User page" + req.startTime)
 })
 
 app.listen(80, () => {
