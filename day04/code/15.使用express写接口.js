22c63dba9c323eb9a8ea975cdbd631402c7c1086
乐天居易 <19836788252@163.com>
2024-08-11 22:46:57 +0800

    编写接口-创建基本的服务器&创建API路由模块

---
+++ day04/code/15.使用express写接口.js
@@ -0,0 +1,13 @@
+// 导入express模块;
+const express = require("express")
+// 创建express的服务器实例;
+const app = express()
+
+// 导入路由模块
+const router = require("./16.apiRouter")
+// 把路由模块，注册到app上
+app.use("/api", router)
+// 调用app.listen方法，在指定的80端口启动web服务器;
+app.listen(80, () => {
+  console.log("Express server running at http:127.0.0.1")
+})
