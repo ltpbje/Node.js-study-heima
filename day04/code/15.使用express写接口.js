688b627393717eaf0673a1ec95d71c3c6eb6d2ae
乐天居易 <19836788252@163.com>
2024-08-12 16:07:28 +0800

    编写接口-基于cors解决接口跨域问题

--- day04/code/15.使用express写接口.js
+++ day04/code/15.使用express写接口.js
@@ -4,6 +4,11 @@ const express = require("express")
 const app = express()
 // 配置解析表单数的中间件
 app.use(express.urlencoded({ extended: false }))
+
+// 一定要在路由之前，配置cors这个中间件，从而解决接口跨域的问题
+const cros = require("cors")
+app.use(cros())
+
 // 导入路由模块
 const router = require("./16.apiRouter")
 // 把路由模块，注册到app上
