1da6e8e7344800a03a25f1cb9cb9d024366fee0b
乐天居易 <19836788252@163.com>
2024-08-13 12:23:22 +0800

    跨域-编写jsonp接口

--- day04/code/15.使用express写接口.js
+++ day04/code/15.使用express写接口.js
@@ -5,6 +5,19 @@ const app = express()
 // 配置解析表单数的中间件
 app.use(express.urlencoded({ extended: false }))
 
+// 优先创建JSONP接口【这个接口不会被处理成CORS接口】
+app.get("/api/jsonp", (req, res) => {
+  // TODO：定义JSONP接口具体的实现过程
+  //1.得到函数的名称
+  const funcName = req.query.callback
+  //2.定义要发送到客户端的数据对象
+  const data = { name: "zs", age: 22 }
+  //3.拼接出一个函数的调用
+  const scriptStr = `${funcName}(${JSON.stringify(data)})`
+  //4.把拼接的字符串，响应给客户端
+  res.send(scriptStr)
+})
+
 // 一定要在路由之前，配置cors这个中间件，从而解决接口跨域的问题
 const cros = require("cors")
 app.use(cros())
