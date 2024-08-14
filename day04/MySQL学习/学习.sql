3122f9b0236e9940be2e1271efa2959ce4ad570b
乐天居易 <19836788252@163.com>
2024-08-14 15:49:50 +0800

    SQL语句-update语句

--- day04/MySQL学习/学习.sql
+++ day04/MySQL学习/学习.sql
@@ -2,4 +2,8 @@
 -- select * from users
 
 -- 从users 表中把usernamee和password对应的数据查询出来
-select username,password from users
+-- select username,password from users
+
+-- 向users 表中，插入新数据，username 的值为tony stark  password的值为098123
+-- insert into users(username,password) values('tony stark','098123')
+select * from users
