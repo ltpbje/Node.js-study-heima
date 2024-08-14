3a5c22e18b4994e145d9623d98a510556763043f
乐天居易 <19836788252@163.com>
2024-08-14 16:21:53 +0800

    UPDATE示例-更新某一行中的若干列

--- day04/MySQL学习/学习.sql
+++ day04/MySQL学习/学习.sql
@@ -6,4 +6,13 @@
 
 -- 向users 表中，插入新数据，username 的值为tony stark  password的值为098123
 -- insert into users(username,password) values('tony stark','098123')
+-- select * from users
+
+-- 将id为4的用户密码，更新成888888
+-- update users set password = '888888' where id = 4
+-- select * from users
+
+
+-- 更新id为2的用户，把用户密码更新为admin123同时，把用户的状态更新为1
+-- update users set password='admin123',status = 1 where id =2
 select * from users
