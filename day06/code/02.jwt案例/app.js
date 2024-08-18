06c7e378375f630a821d4137265764783d6807bd
乐天居易 <19836788252@163.com>
2024-08-18 22:17:45 +0800

    jwt-在express中生成token

--- day06/code/02.jwt案例/app.js
+++ day06/code/02.jwt案例/app.js
@@ -7,19 +7,25 @@ const app = express()
 // 1.导入用于生成JWT(JSON Web Tokens)字符串的包
 const jwt = require("jsonwebtoken")
 // 2.导入用于将客户端发送过来的JWT字符串，解析还原成JSON对象的包
-const expressJWT = require("express-jwt")
+const { expressjwt } = require("express-jwt")
 // 允许跨域资源共享
 const cors = require("cors")
 app.use(cors())
 
 // 解析 post 表单数据的中间件
 const bodyParser = require("body-parser")
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // TODO_02：定义 secret 密钥，建议将密钥命名为 secretKey
 const secretKey = "itheima No1 ^_^"
 // TODO_04：注册将 JWT 字符串解析还原成 JSON 对象的中间件
-
+// app.use(expressjwt({ secret: secretKey }).unless({ path: [/^\/api\//] }))
+app.use(
+  expressjwt({ secret: secretKey, algorithms: ["HS256"] }).unless({
+    path: [/^\/api\//],
+  })
+)
 // 登录接口
 app.post("/api/login", function (req, res) {
   // 将 req.body 请求体中的数据，转存为 userinfo 常量
@@ -36,23 +42,26 @@ app.post("/api/login", function (req, res) {
   //  参数1：用户的信息对象
   //  参数2：加密的秘钥
   //  参数3：配置对象，可以配置当前token的有效期
+  // 记住：千万不要把密码加密到token字符中
   const tokenStr = jwt.sign({ username: userinfo.username }, secretKey, {
-    expiresIn: "30s",
+    expiresIn: "1h",
   })
   res.send({
     status: 200,
     message: "登录成功！",
-    token: "", // 要发送给客户端的 token 字符串
+    token: tokenStr, // 要发送给客户端的 token 字符串
   })
 })
 
 // 这是一个有权限的 API 接口
 app.get("/admin/getinfo", function (req, res) {
   // TODO_05：使用 req.user 获取用户信息，并使用 data 属性将用户信息发送给客户端
+  console.log(req.auth)
+
   res.send({
     status: 200,
     message: "获取用户信息成功！",
-    data: {}, // 要发送给客户端的用户信息
+    data: req.auth, // 要发送给客户端的用户信息
   })
 })
 
