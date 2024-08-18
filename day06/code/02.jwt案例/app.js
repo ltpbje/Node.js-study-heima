1a059eaf88baef26827399d41841261f0f02f03e
乐天居易 <19836788252@163.com>
2024-08-18 16:50:08 +0800

    在登录成功后生成JWT字符串

--- day06/code/02.jwt案例/app.js
+++ day06/code/02.jwt案例/app.js
@@ -1,49 +1,58 @@
 // 导入 express 模块
-const express = require('express')
+const express = require("express")
 // 创建 express 的服务器实例
 const app = express()
 
 // TODO_01：安装并导入 JWT 相关的两个包，分别是 jsonwebtoken 和 express-jwt
-
+// 1.导入用于生成JWT(JSON Web Tokens)字符串的包
+const jwt = require("jsonwebtoken")
+// 2.导入用于将客户端发送过来的JWT字符串，解析还原成JSON对象的包
+const expressJWT = require("express-jwt")
 // 允许跨域资源共享
-const cors = require('cors')
+const cors = require("cors")
 app.use(cors())
 
 // 解析 post 表单数据的中间件
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // TODO_02：定义 secret 密钥，建议将密钥命名为 secretKey
-
+const secretKey = "itheima No1 ^_^"
 // TODO_04：注册将 JWT 字符串解析还原成 JSON 对象的中间件
 
 // 登录接口
-app.post('/api/login', function (req, res) {
+app.post("/api/login", function (req, res) {
   // 将 req.body 请求体中的数据，转存为 userinfo 常量
   const userinfo = req.body
   // 登录失败
-  if (userinfo.username !== 'admin' || userinfo.password !== '000000') {
+  if (userinfo.username !== "admin" || userinfo.password !== "000000") {
     return res.send({
       status: 400,
-      message: '登录失败！'
+      message: "登录失败！",
     })
   }
   // 登录成功
   // TODO_03：在登录成功之后，调用 jwt.sign() 方法生成 JWT 字符串。并通过 token 属性发送给客户端
+  //  参数1：用户的信息对象
+  //  参数2：加密的秘钥
+  //  参数3：配置对象，可以配置当前token的有效期
+  const tokenStr = jwt.sign({ username: userinfo.username }, secretKey, {
+    expiresIn: "30s",
+  })
   res.send({
     status: 200,
-    message: '登录成功！',
-    token: '' // 要发送给客户端的 token 字符串
+    message: "登录成功！",
+    token: "", // 要发送给客户端的 token 字符串
   })
 })
 
 // 这是一个有权限的 API 接口
-app.get('/admin/getinfo', function (req, res) {
+app.get("/admin/getinfo", function (req, res) {
   // TODO_05：使用 req.user 获取用户信息，并使用 data 属性将用户信息发送给客户端
   res.send({
     status: 200,
-    message: '获取用户信息成功！',
-    data: {} // 要发送给客户端的用户信息
+    message: "获取用户信息成功！",
+    data: {}, // 要发送给客户端的用户信息
   })
 })
 
@@ -51,5 +60,5 @@ app.get('/admin/getinfo', function (req, res) {
 
 // 调用 app.listen 方法，指定端口号并启动web服务器
 app.listen(8888, function () {
-  console.log('Express server running at http://127.0.0.1:8888')
+  console.log("Express server running at http://127.0.0.1:8888")
 })
