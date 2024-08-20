03f393f2c8f1af9801152e50a89c7933f7a56c5f
乐天居易 <19836788252@163.com>
2024-08-20 16:02:32 +0800

    判断用户名和密码是否为空

--- day06/code/api_server/app.js
+++ day06/code/api_server/app.js
@@ -7,12 +7,13 @@ const cors = require("cors")
 // 将 cors 注册为全局中间件
 app.use(cors())
 
+// 配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
+app.use(express.urlencoded({ extended: false }))
+
 // 导入并注册用户路由模块
 const userRouter = require("./router/user")
 app.use("/api", userRouter)
 
-// 配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
-app.use(express.urlencoded({ extended: false }))
 // 调用app.listen方法，在指定的80端口启动web服务器;
 app.listen(3007, () => {
   console.log("api server running at http//:127.0.0.1:3007")
--- day06/code/api_server/router_handlers/user.js
+++ day06/code/api_server/router_handlers/user.js
@@ -1,4 +1,15 @@
 exports.regUser = (req, res) => {
+  // 接收表单数据
+  const userinfo = req.body
+  console.log(userinfo)
+
+  // 判断数据是否合法
+  if (!userinfo.username || !userinfo.password) {
+    return res.send({
+      status: 1,
+      message: "用户名或密码不能为空",
+    })
+  }
   res.send("reguser OK")
 }
 exports.login = (req, res) => {
