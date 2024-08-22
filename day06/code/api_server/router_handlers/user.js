dea6a63df1fb2fa15338da40c0e88bd55285b265
乐天居易 <19836788252@163.com>
2024-08-22 16:15:19 +0800

    插入新用户

--- day06/code/api_server/router_handlers/user.js
+++ day06/code/api_server/router_handlers/user.js
@@ -29,9 +29,25 @@ exports.regUser = (req, res) => {
     // 用户名可以使用
   })
   // 调用bcrypt.hashSync()对密码进行加密
-  console.log(userinfo)
+  // console.log(userinfo)
   userinfo.password = bcryptjs.hashSync(userinfo.password, 10)
-  console.log(userinfo)
+  // console.log(userinfo)
+
+  // 定义插入用户的 SQL 语句：
+  const sql = "insert into ev_users set ?"
+  // 调用 `db.query()` 执行 SQL 语句，插入新用户：
+  db.query(
+    sql,
+    { username: userinfo.username, password: userinfo.password },
+    (err, results) => {
+      // 执行 SQL 语句失败
+      if (err) return res.send({ status: 1, message: err.message })
+      if (results.affectedRows !== 1)
+        return res.send({ status: 1, message: "注册用户失败，请稍后再试！" })
+      // 注册成功
+      res.send({ status: 0, message: "注册成功！" })
+    }
+  )
 }
 exports.login = (req, res) => {
   res.send("login OK")
