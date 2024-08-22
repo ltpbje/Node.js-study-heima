8a1c9c558e2942be20563531546f5d7d03791c85
乐天居易 <19836788252@163.com>
2024-08-22 13:58:00 +0800

    对密码进行加密处理

--- day06/code/api_server/router_handlers/user.js
+++ day06/code/api_server/router_handlers/user.js
@@ -1,10 +1,11 @@
 // 导入数据库操作模块
 const db = require("../db/index")
-
+//导入bcryptjs这个包
+const bcryptjs = require("bcryptjs")
 exports.regUser = (req, res) => {
   // 接收表单数据
   const userinfo = req.body
-  console.log(userinfo)
+  // console.log(userinfo)
 
   // 判断数据是否合法
   if (!userinfo.username || !userinfo.password) {
@@ -16,7 +17,7 @@ exports.regUser = (req, res) => {
 
   //定义SQL语句，查询用户名是否被占用
   const sqlStr = "select * from ev_users where username = ?"
-  db.query(sqlStr, userinfo.username, (err, results) => {
+  db.query(sqlStr, userinfo.username, function (err, results) {
     // 执行 SQL 语句失败
     if (err) {
       return res.send({ status: 1, message: err.message })
@@ -27,7 +28,10 @@ exports.regUser = (req, res) => {
     }
     // 用户名可以使用
   })
-  res.send("reguser OK")
+  // 调用bcrypt.hashSync()对密码进行加密
+  console.log(userinfo)
+  userinfo.password = bcryptjs.hashSync(userinfo.password, 10)
+  console.log(userinfo)
 }
 exports.login = (req, res) => {
   res.send("login OK")
