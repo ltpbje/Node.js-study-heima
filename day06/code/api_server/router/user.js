42ffaf073cd906df29bfbfcfe2572961720e5bda
乐天居易 <19836788252@163.com>
2024-08-24 09:32:26 +0800

    检测登录表单的数据是否合法

--- day06/code/api_server/router/user.js
+++ day06/code/api_server/router/user.js
@@ -15,7 +15,7 @@ const { reg_login_schema } = require("../schema/user")
 router.post("/reguser", expressJoi(reg_login_schema), userHandler.regUser)
 
 // 登录
-router.post("/login", userHandler.login)
+router.post("/login", expressJoi(reg_login_schema), userHandler.login)
 
 // 将路由对象共享出去
 module.exports = router
