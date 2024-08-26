6627c47220d4faf40bb4864e222fe1244d19ff46
乐天居易 <19836788252@163.com>
2024-08-26 16:06:00 +0800

    实现重置密码的功能

--- day06/code/api_server/router_handlers/userinfo.js
+++ day06/code/api_server/router_handlers/userinfo.js
@@ -1,5 +1,9 @@
 // 导入数据库操作模块
 const db = require("../db/index")
+// 在头部区域导入 bcryptjs 后，
+// 即可使用 bcrypt.compareSync(提交的密码，数据库中的密码) 方法验证密码是否正确
+// compareSync() 函数的返回值为布尔值，true 表示密码正确，false 表示密码错误
+const bcrypt = require("bcryptjs")
 
 // 获取用户基本信息的处理函数
 exports.getUserInfo = (req, res) => {
@@ -37,5 +41,38 @@ exports.updateUserInfo = (req, res) => {
 
 // 更新用户的密码
 exports.updatePassword = (req, res) => {
-  res.send("ok")
+  // 定义根据 id 查询用户数据的 SQL 语句
+  const sql = "select * from ev_users where id=?"
+
+  // 执行 SQL 语句查询用户是否存在
+  db.query(sql, req.user.id, (err, results) => {
+    // 执行 SQL 语句失败
+    if (err) return res.cc(err)
+    // 检查指定 id 的用户是否存在
+    if (results.length !== 1) return res.cc("用户不存在!")
+
+    // 判断提交的旧密码是否正确
+    const compareResult = bcrypt.compareSync(
+      req.body.oldPwd,
+      results[0].password
+    )
+    if (!compareResult) return res.cc("原密码错误")
+
+    // 更新数据库中的密码
+    // 定义更新用户密码的 SQL 语句
+    const sql = "update ev_users set password = ? where id =?"
+
+    // 对新密码进行 bcrypt 加密处理
+    const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+
+    // 执行 SQL 语句，根据 id 更新用户的密码
+    db.query(sql, [newPwd, req.user.id], (err, results) => {
+      // SQL 语句执行失败
+      if (err) return res.cc(err)
+      // SQL 语句执行成功，但是影响行数不等于 1
+      if (results.affectedRows !== 1) return res.cc("更新密码失败!")
+      // 更新密码成功
+      res.cc("修改密码成功", 0)
+    })
+  })
 }
