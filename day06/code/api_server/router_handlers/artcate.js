3349ecbf02008cd44e814f0281d9649b0944cc09
乐天居易 <19836788252@163.com>
2024-09-01 10:50:22 +0800

    查询分类名称与别名是否被占用

--- day06/code/api_server/router_handlers/artcate.js
+++ day06/code/api_server/router_handlers/artcate.js
@@ -22,5 +22,26 @@ exports.getArticleCates = (req, res) => {
 
 // 新增文章分类的处理函数
 exports.addArticleCates = (req, res) => {
-  res.send("ok")
+  // 定义查询 分类名称 与 分类别名 是否被占用的 SQL 语句
+  const sql = "select * from ev_article_cate where name = ? or alias = ?"
+  // 执行查重操作
+  db.query(sql, [req.body.name, req.body.alias], (err, results) => {
+    if (err) return res.cc(err)
+    // 判断 分类名称 和 分类别名 是否被占用
+    //4.1判断数据的length
+    if (results.length === 2)
+      return res.cc("分类名称和分类别名分别被占用 请更换后重试")
+    if (results.length === 1 && results[0].name === req.body.name)
+      return res.cc("分类名称被占用 请更换后重试")
+    if (results.length === 1 && results[0].alias === req.body.alias)
+      return res.cc("分类别名被占用 请更换后重试")
+    if (
+      results.length === 1 &&
+      results[0].alias === req.body.alias &&
+      results[0].name === req.body.name
+    )
+      return res.cc("分类名称和分类别名分别被占用 请更换后重试")
+
+    //TODO：分类名称和分类别名都可用，执行添加的动作
+  })
 }
